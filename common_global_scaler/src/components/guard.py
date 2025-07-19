fix: solve scaling problem

common_global_scaler/src/components/guard.py
@@ -30,7 +30,7 @@ class Guard:
         self.mixer = mixer
 
         prometheus_service_address = os.environ.get("PROMETHEUS_SERVICE_ADDRESS", "localhost")
-        prometheus_service_port = os.environ.get("PROMETHEUS_SERVICE_PORT", "64155")
+        prometheus_service_port = os.environ.get("PROMETHEUS_SERVICE_PORT", "64671")
 
         self.monitor_only = os.environ.get("MONITOR_ONLY", "false").lower() == 'true'
         prometheus_url = f"http://{prometheus_service_address}:{prometheus_service_port}"
@@ -85,40 +85,49 @@ class Guard:
             pred_workload = sum(self.predictions[iter-self.sleep:])/self.sleep
             last_pred_conf = self.scaler.calculate_configuration(pred_workload + self.k_big)
             current_mcl, _ = self.scaler.process_request(last_pred_conf)
-
-        while self.running:        
+        
+        sleep_time = 1
+        while self.running:
             tot = self._execute_prometheus_query(f"sum(increase(http_requests_total_webUI_counter[{self.sleep}s]))")
-            completed = self._execute_prometheus_query(f"sum(increase(behaviour_execution[{self.sleep}s]))")
+            completed = self._execute_prometheus_query(f"sum(increase(behaviour_execution[{self.sleep}s]))")    
             latency = self._execute_prometheus_query(f"sum(increase(behaviour_time_execution[{self.sleep}s]))")
-            avg_lat = latency/(completed if completed > 0 else 1)
+            avg_lat = (latency if latency is not None else 0.0)/(completed if (completed is not None and completed > 0) else 1)
             loss = self._execute_prometheus_query(f"sum(increase(message_lost_webUI[{self.sleep}s]))")
             toPrint = str(iter) + " " + str(avg_lat)
-              
 
-            #reactivity
-            measured_workload = tot/self.sleep#(tot-init_val)/self.sleep
-            target_workload = measured_workload
-            
-            #proactivity
-            if iter > 0 and self.proactiveness:
-                diff = iter-self.sleep
-                pred_workload = sum(self.predictions[diff if diff > 0 else 0:iter])/self.sleep
-                target_workload = pred_workload
-            if self.proactiveness: toPrint += " next: " + str(pred_workload)
-            toPrint += " measured: " + str(measured_workload)
-            config = np.sum(self.scaler.get_current_config()) if not self.monitor_only else self._execute_prometheus_query("sum(total_instances_number)")
-            #proactivity + reactivity:
-            if iter > 0 and self.proactive_reactive:
-                measured_conf = self.scaler.calculate_configuration(measured_workload + self.k_big)
-                target_workload = self.mixer.mix(measured_workload, pred_workload, last_pred_conf, measured_conf)
-                last_pred_conf = self.scaler.calculate_configuration(pred_workload + self.k_big)
-            if self.proactive_reactive: toPrint += " mixed: " + str(target_workload)
-            toPrint += " tot: " + str(measured_workload * self.sleep) + " comp: " + str(completed) + " rej: " + str(loss) + " supp: " + str(current_mcl) + " inst: " + str(config)
-            print(toPrint, flush=True)
+            if tot is not None and (tot > 0 or iter > 0): 
+                sleep_time = self.sleep
+                
+                #reactivity
+                measured_workload = (tot if tot is not None else 0.0)/self.sleep#(tot-init_val)/self.sleep
+                target_workload = measured_workload
+                
+                #proactivity
+                if iter > 0 and self.proactiveness:
+                    diff = iter-self.sleep
+                    pred_workload = sum(self.predictions[diff if diff > 0 else 0:iter])/self.sleep
+                    target_workload = pred_workload
+                if self.proactiveness: toPrint += " next: " + str(pred_workload)
+                toPrint += " measured: " + str(measured_workload)
+                config = np.sum(self.scaler.get_current_config()) if not self.monitor_only else self._execute_prometheus_query("sum(total_instances_number)")
+                #proactivity + reactivity:
+                if iter > 0 and self.proactive_reactive:
+                    measured_conf = self.scaler.calculate_configuration(measured_workload + self.k_big)
+                    target_workload = self.mixer.mix(measured_workload, pred_workload, last_pred_conf, measured_conf)
+                    last_pred_conf = self.scaler.calculate_configuration(pred_workload + self.k_big)
+                if self.proactive_reactive: toPrint += " mixed: " + str(target_workload)
+                toPrint += " tot: " + str(measured_workload * self.sleep) + " comp: " + str(completed) + " rej: " + str(loss) + " supp: " + str(current_mcl) + " inst: " + str(config)
+                print(toPrint, flush=True)
 
-            if self.should_scale(target_workload, current_mcl) and not self.monitor_only:
-                target_conf = self.scaler.calculate_configuration(target_workload + self.k_big)
-                current_mcl, _ = self.scaler.process_request(target_conf)    
+                if self.should_scale(target_workload, current_mcl) and not self.monitor_only:
+                    target_conf = self.scaler.calculate_configuration(target_workload + self.k_big)
+                    current_mcl, _ = self.scaler.process_request(target_conf)    
 
-            iter += self.sleep
-            time.sleep(self.sleep)
+                iter += self.sleep
+        
+            else: 
+                toPrint += " measured: 0.0 tot: 0 comp: 0 rej: 0 supp: " + str(current_mcl) + " inst: 0"
+                iter = 0 
+                print(toPrint, flush=True)
+
+            time.sleep(sleep_time)